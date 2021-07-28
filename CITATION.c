150f560dfb6ee38da00dd5acfac4ffafad3fe66c
Alex Harston <alex@harston.io>
2021-07-28 12:59:39 +0100

    add citation file

---
+++ CITATION.c
@@ -0,0 +1,10 @@
+cff-version: 1.0.0
+message: "If you use this software, please cite it as below."
+authors:
+  - family-names: Harston
+    given-names: Alex
+    orcid: https://orcid.org/0000-0002-3261-4088
+title: "MVNX - A Python MVNX File Parser in"
+version: 1.0.0
+date-released: 2021-07-28
+url: https://github.com/alexharston/mvnx
