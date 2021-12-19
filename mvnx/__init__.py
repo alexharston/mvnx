3d8aa6c46a4948e9b72d19d34aa3a4d72a07ca01
Alex Harston <alex@harston.io>
2021-12-19 01:28:24 +0000

    add compression option and better error logging

--- mvnx/__init__.py
+++ mvnx/__init__.py
@@ -4,6 +4,7 @@ import argparse
 import errno
 import os
 import warnings
+import traceback
 
 def load(*args, **kwargs):
     return MVNX(*args, **kwargs)
@@ -14,26 +15,35 @@ def main():
     parser.add_argument("-f", "--file", help="the MVNX file to parse")
     parser.add_argument("-m", "--modality", help="the modality to parse")
     parser.add_argument("-o", "--output", help="filepath to save parsed data to (saves as .npy)")
+    parser.add_argument("-c", "--compress", help="Compress the saved object")
     args = parser.parse_args()
+    if not args.output:
+        args.output = f'{args.file.split(".")[0]}.npy'
     if (args.file == None and args.modality == None):
         parser.print_help()
     else:
         try:
             if args.file:
-                print(f'{args.file} selected - writing MVNX to {args.output}')
+                print(f'Writing {args.file} to {args.output}')
                 mvnx = MVNX(args.file)
                 if args.modality:
                     modality = mvnx.parse_modality(args.modality)
-                    np.save(args.output, modality) 
+                    if args.compress:
+                        np.savez_compressed(args.output, modality) 
+                    else:
+                        np.save(args.output, modality)
                 elif args.output:
-                    np.save(args.output, mvnx)
+                    if args.compress:
+                        np.savez_compressed(args.output, mvnx)
+                    else:
+                        np.save(args.output, mvnx)
                 else:
                     warnings.warn('No output location selected, printing to terminal instead')
             else:
                 raise FileNotFoundError(
                     errno.ENOENT, os.strerror(errno.ENOENT), args.file)
-        except Exception:
-            print('Something went wrong!')
+        except Exception as e:
+            traceback.print_exc()
 
 if __name__ == "__main__":
     main()
--- mvnx/models.py
+++ mvnx/models.py
@@ -1,5 +1,6 @@
 import numpy as np
 import xml.etree.ElementTree as ET
+from tqdm import tqdm
 
 
 class MVNX:
@@ -144,7 +145,7 @@ class MVNX:
 
         holding_list = []
         frames = self.root[2][6]
-        for frame in frames[3:]:
+        for frame in tqdm(frames[3:]):
             for child in frame[self.mapping[modality]:self.mapping[modality]+1]:
                 holding_list.append(child.text.split(' '))           
         holding_list = np.array(holding_list)
@@ -152,50 +153,50 @@ class MVNX:
 
     def parse_time(self):
         frames = self.root[2][6][3:]
-        for frame in frames:
+        for frame in tqdm(frames):
             self.time.append(frame.attrib['time'])
         return self.time
     
     def parse_index(self):
         frames = self.root[2][6][3:]
-        for frame in frames:
+        for frame in tqdm(frames):
             self.index.append(frame.attrib['index'])
         return self.index
     
     def parse_timecode(self):
         frames = self.root[2][6][3:]
-        for frame in frames:
+        for frame in tqdm(frames):
             self.timecode.append(frame.attrib['tc'])
         return self.timecode
     
     def parse_ms(self):
         frames = self.root[2][6][3:]
-        for frame in frames:
+        for frame in tqdm(frames):
             self.ms.append(frame.attrib['ms'])
         return self.ms
 
     def parse_modalities(self, *args):
-        for arg in args:
+        for arg in tqdm(args):
             print(self.parse_modality(arg))
             return self.parse_modality(arg)
 
     def parse_sensors(self):
-        for sensor in self.root[2][2]:
+        for sensor in tqdm(self.root[2][2]):
             self.sensors.append(sensor.attrib['label'])
         return self.sensors
     
     def parse_segments(self):
-        for segment in self.root[2][1]:
+        for segment in tqdm(self.root[2][1]):
             self.segments[segment.attrib['id']] = segment.attrib['label']
         return self.segments
     
     def parse_joints(self):
-        for joint in self.root[2][3]:
+        for joint in tqdm(self.root[2][3]):
             self.joints[joint.attrib['label']] = [joint[0].text, joint[1].text]
         return self.joints
 
     def parse_all(self):
-        for key in self.mapping.keys():
+        for key in tqdm(self.mapping.keys()):
             setattr(self, key, self.parse_modality(key))
         self.parse_time()
         self.parse_joints()
